d737184bb214c46e9d103752da51fc469bd1d4dc
WILLIAM <wsleal@yahoo.com>
2021-12-06 15:47:25 -0300

    .

--- make_conf.py
+++ make_conf.py
@@ -7,8 +7,10 @@ if __name__ == "__main__":  # pragma: no cover
 
     if getattr(sys, "frozen", False) and hasattr(sys, "_MEIPASS"):
         path = Path(sys.executable).parent
+        duck_ext = "duckdns.exe"
     else:
         path = Path(__file__).parent
+        duck_ext = "duckdns.py"
 
     if not os.path.isfile(Path(path, "config.json")):
         duck_conf = {}
@@ -17,3 +19,4 @@ if __name__ == "__main__":  # pragma: no cover
         duck_conf["delay"] = int(input("Delay (em segundos): "))
         with open(Path(path, "config.json"), "w") as conf:
             json.dump(duck_conf, conf, indent=4)
+        os.startfile(Path(path, duck_conf))
