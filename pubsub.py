4da9e351924733a36c68dc181f8494a257a6e6b2
William Leal <wsleal@yahoo.com>
2021-12-09 19:12:02 -0300

    .

---
+++ pubsub.py
@@ -0,0 +1,47 @@
+from threading import Thread
+
+
+class Broker:
+    def __init__(self) -> None:
+        self.eventlisteners = []
+
+    def add_eventlistener(self, eventlistener):
+        if eventlistener not in self.eventlisteners:
+            self.eventlisteners.append(eventlistener)
+
+    def remove_eventlistener(self, eventlistener):
+        if eventlistener in self.eventlisteners:
+            self.eventlisteners.remove(eventlistener)
+
+    def router(self, context, topic: str):
+        for eventlistener in self.eventlisteners:
+            if topic == eventlistener.topic:
+                thread = Thread(target=eventlistener.update, args=(context,))
+                thread.start()
+
+
+class Event:
+    def __init__(self, broker: Broker) -> None:
+        self.broker = broker
+
+    def notify(self, context, topic: str):
+        self.broker.router(context, topic)
+
+
+class EventListener:
+    def __init__(self, broker: Broker, topic: str, callback) -> None:
+        broker.add_eventlistener(self)
+        self.topic = topic
+        self.callback = callback
+
+    def update(self, context):
+        self.callback(context)
+
+
+if __name__ == "__main__":
+
+    broker = Broker()
+    escuta_msg = EventListener(broker=broker, topic="musica", callback=print)
+
+    msg = Event(broker=broker)
+    msg.notify("Do Re Mi", topic="musica")
